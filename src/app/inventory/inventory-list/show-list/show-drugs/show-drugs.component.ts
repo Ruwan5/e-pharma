f063ca7b012c08fce258063f4350a25721aad812
Ruwan <ruwannawarathna96@gmail.com>
2020-01-25 23:47:37 +0530

    New component

---
+++ src/app/inventory/inventory-list/show-list/show-drugs/show-drugs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-drugs',
+  templateUrl: './show-drugs.component.html',
+  styleUrls: ['./show-drugs.component.scss']
+})
+export class ShowDrugsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
